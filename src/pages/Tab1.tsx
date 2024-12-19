acd422c6ea0e59e72f9be5816060bd6180e4d00c
naikibro <naikibro@gmail.com>
2024-12-19 04:45:44 -1000

    feat: luia tab1

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,6 +1,12 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Tab1.css';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import ExploreContainer from "../components/ExploreContainer";
+import "./Tab1.css";
 
 const Tab1: React.FC = () => {
   return (
@@ -17,6 +23,10 @@ const Tab1: React.FC = () => {
           </IonToolbar>
         </IonHeader>
         <ExploreContainer name="Tab 1 page" />
+        <div>
+          <h2>LUIA</h2>
+          <h3>Ceci est une PWA ma gueule</h3>
+        </div>
       </IonContent>
     </IonPage>
   );
